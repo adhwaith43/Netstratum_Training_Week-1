67fd045a693a6eff6ff7dfe36da78d9074504b82
Adhwaith A M <adhwaith43@gmail.com>
2026-03-09 12:17:09 +0530

    commmit 2

--- DjangoRestWorks/libraryapi/books/views.py
+++ DjangoRestWorks/libraryapi/books/views.py
@@ -20,17 +20,37 @@ from books.models import Book
 #             return Response(book.data,status=status.HTTP_201_CREATED)
 
 
-# # using mixins
+# # # using mixins
 from rest_framework import mixins,generics
 
-class BookList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+# class BookList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+#     queryset=Book.objects.all()
+#     serializer_class=BookSerializer
+
+#     def get(self,request):
+#         return self.list(request)
+#     def post(self,request):
+#         return self.create(request)
+    
+
+# using generics
+
+class BookList(generics.ListCreateAPIView):
     queryset=Book.objects.all()
     serializer_class=BookSerializer
 
-    def get(self,request):
-        return self.list(request)
-    def post(self,request):
-        return self.create(request)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # # using api views
@@ -61,19 +81,26 @@ class BookList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPI
 #         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# mixins
-class BookDetail(mixins.RetrieveModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 generics.GenericAPIView):
+# # mixins
+# class BookDetail(mixins.RetrieveModelMixin,
+#                  mixins.UpdateModelMixin,
+#                  mixins.DestroyModelMixin,
+#                  generics.GenericAPIView):
+#     queryset=Book.objects.all()
+#     serializer_class = BookSerializer
+
+#     def get(self,request,pk):
+#         return self.retrieve(request,pk)
+
+#     def put(self,request,pk):
+#         return self.update(request,pk)
+
+#     def delete(self,request,pk):
+#         return self.destroy(request,pk)
+
+
+# using generics
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset=Book.objects.all()
     serializer_class = BookSerializer
-
-    def get(self,request,pk):
-        return self.retrieve(request,pk)
-
-    def put(self,request,pk):
-        return self.update(request,pk)
-
-    def delete(self,request,pk):
-        return self.destroy(request,pk)
