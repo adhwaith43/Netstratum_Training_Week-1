8700d02e5ff79f144cddd76dc91c67ce188e802e
Adhwaith A M <adhwaith43@gmail.com>
2026-03-11 10:25:03 +0530

    main

--- movieapp/src/main.jsx
+++ movieapp/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { Provider } from "react-redux";
+import { store, persistor } from "./app/store";
+
+import { PersistGate } from "redux-persist/integration/react";
+
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+<Auth0Provider
+domain="YOUR_AUTH0_DOMAIN"
+clientId="YOUR_CLIENT_ID"
+authorizationParams={{
+redirect_uri: window.location.origin
+}}
+>
+
+<Provider store={store}>
+
+<PersistGate loading={null} persistor={persistor}>
+
+<App/>
+
+</PersistGate>
+
+</Provider>
+
+</Auth0Provider>
+
+);
