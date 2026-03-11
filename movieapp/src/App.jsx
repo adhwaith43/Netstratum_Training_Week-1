201fd6cec9a51445305bb59a57355c4b59770ede
Adhwaith A M <adhwaith43@gmail.com>
2026-03-11 10:25:54 +0530

    app

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import Search from "./pages/Search";
+import Favorites from "./pages/Favorites";
+import Profile from "./pages/Profile";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./auth/ProtectedRoute";
+
+function App(){
+
+return(
+
+<BrowserRouter>
+
+<Navbar/>
+
+<Routes>
+
+<Route path="/" element={<Home/>}/>
+
+<Route path="/details/:type/:id" element={<Details/>}/>
+
+<Route path="/search/:query" element={<Search/>}/>
+
+<Route path="/profile" element={<Profile/>}/>
+
+<Route
+path="/favorites"
+element={
+<ProtectedRoute>
+<Favorites/>
+</ProtectedRoute>
+}
+/>
+
+</Routes>
+
+</BrowserRouter>
+
+)
+
 }
 
-export default App
+export default App;
